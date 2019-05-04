e16ddd11acb711248d18e4564e1703c1f8a1757c
Nate Morris <nate.morris@marconet.com>
2019-05-03 20:26:18 -0500

    Update backup times

--- backup/setup.sh
+++ backup/setup.sh
@@ -8,12 +8,12 @@ sudo mkdir /srv/backup/gracemn.com 2>/dev/null
 sudo mkdir /srv/backup/members.gracemn.com 2>/dev/null
 
 # Wordpress data
-sudo echo "00 00 * * *    root    tar -cvpzf /srv/backup/gracemn.com/\`date +\\%a\`.tar.gz -C /srv/wordpress gracemn.com" | sudo tee /etc/cron.d/gracemn
-sudo echo "05 00 * * *    root    tar --exclude='members.gracemn.com/uploads' -cvpzf /srv/backup/members.gracemn.com/\`date +\\%a\`.tar.gz -C /srv/wordpress members.gracemn.com" | sudo tee /etc/cron.d/members
+sudo echo "15 00 * * *    root    tar -cvpzf /srv/backup/gracemn.com/\`date +\\%a\`.tar.gz -C /srv/wordpress gracemn.com" | sudo tee /etc/cron.d/gracemn
+sudo echo "20 00 * * *    root    tar --exclude='members.gracemn.com/uploads' -cvpzf /srv/backup/members.gracemn.com/\`date +\\%a\`.tar.gz -C /srv/wordpress members.gracemn.com" | sudo tee /etc/cron.d/members
 
 # Wordpress uploads for members site
-sudo echo "10 00 21 * *    root    tar -cvpzf /srv/backup/members.gracemn.com/uploads-\$((\`date +\\%m\`%2)).tar.gz -C /srv/wordpress members.gracemn.com/uploads" | sudo tee -a /etc/cron.d/members
+sudo echo "00 01 1 * *    root    tar -cvpzf /srv/backup/members.gracemn.com/uploads.tar.gz -C /srv/wordpress members.gracemn.com/uploads" | sudo tee -a /etc/cron.d/members
 
 # MariaDB data
-sudo echo "50 23 * * *    root    docker run --rm --net=gracemn_db -u root -v /srv/backup/gracemn.com:/backup mariadb sh -c \"mysqldump -h db -u wordpress --password='$PW1' wordpress > /backup/\`date +\\%a\`.sql\"" | sudo tee -a /etc/cron.d/gracemn
-sudo echo "55 23 * * *    root    docker run --rm --net=members_db -u root -v /srv/backup/members.gracemn.com:/backup mariadb sh -c \"mysqldump -h db -u wordpress --password='$PW2' wordpress > /backup/\`date +\\%a\`.sql\"" | sudo tee -a /etc/cron.d/members
+sudo echo "05 00 * * *    root    docker run --rm --net=gracemn_db -u root -v /srv/backup/gracemn.com:/backup mariadb sh -c \"mysqldump -h db -u wordpress --password='$PW1' wordpress > /backup/\`date +\\%a\`.sql\"" | sudo tee -a /etc/cron.d/gracemn
+sudo echo "10 00 * * *    root    docker run --rm --net=members_db -u root -v /srv/backup/members.gracemn.com:/backup mariadb sh -c \"mysqldump -h db -u wordpress --password='$PW2' wordpress > /backup/\`date +\\%a\`.sql\"" | sudo tee -a /etc/cron.d/members
